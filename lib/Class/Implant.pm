b782850d18b97e3f0322004e7b7ca01cf4c7e82d
shelling <navyblueshellingford@gmail.com>
2009-04-02 19:03:29 +0800

    add MANIFEST.SKIP, some POD

--- lib/Class/Implant.pm
+++ lib/Class/Implant.pm
@@ -1,9 +1,12 @@
 package Class::Implant;
 
+use 5.008008;
 use strict;
 no  strict "refs";
 use warnings;
 use Class::Inspector;
+
+our $VERSION = '0.01_01';
 
 sub import {
   *{(caller)[0] . "::implant"} = \&implant;
@@ -28,3 +31,49 @@ sub implant (@) {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+Class::Implant - Manipulating Mix-In and Inheritance of Packages from outside
+
+=head1 SYNOPSIS
+
+  package Cat;
+  use Class::Implant;
+  implant qw(Foo::Bar), { inherit => 1 }; # import all methods from Foo::Bar and inherit it
+                                          # just like inheritance of ruby
+
+  implant qw(Less::More);                 # mixing all methods from Less::More, 
+                                          # like ruby 'include'
+
+=head1 DESCRIPTION
+
+
+=head2 EXPORT
+
+
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+shelling, E<lt>shelling@apple.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2009 by shelling
+
+MIT(X11) Licence
+
+=cut
