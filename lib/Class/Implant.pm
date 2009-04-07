71ca726ad8c7e94604639af515f47ba1f1f87e9c
shelling <navyblueshellingford@gmail.com>
2009-04-07 23:16:36 +0800

    edit POD

--- lib/Class/Implant.pm
+++ lib/Class/Implant.pm
@@ -38,14 +38,52 @@ sub _get_methods { Class::Inspector->functions(shift) }
 
 =head1 SYNOPSIS
 
+There are two ways to use Class::Implant.
+
+procedural way as follow.
+
+  package main;
+  use Class::Implant;
+
+  implant qw(Foo::Bar Less::More) { into => "Cat" }   # import all methods in Foo::Bar and Less::More into Cat
+
+or in classical way. just using caller as default target for implanting.
+
   package Cat;
   use Class::Implant;
-  implant qw(Foo::Bar), { inherit => 1 }; # import all methods from Foo::Bar and inherit it
-                                          # just like inheritance of ruby
 
   implant qw(Less::More);                 # mixing all methods from Less::More, 
                                           # like ruby 'include'
 
+  implant qw(Foo::Bar), { inherit => 1 }; # import all methods from Foo::Bar and inherit it
+                                          # it just do one more step: unshift Foo::Bar into @ISA
+                                          # this step is trivial in Perl
+                                          # but present a verisimilitude simulation of inheritance in Ruby
+
+
+=head1 SYNTAX
+
+use Class::Implant;
+
+implant @classes_for_injection, { options => value }
+
+available options show as following.
+
+=head2 into
+
+target package for injection.
+
+=head2 inherit
+
+give 1 or any value to mark the inheritance
+
+=head2 include
+
+this option is not available in 0.01
+
+=head2 exclude
+
+this option is not available in 0.01
 
 =head1 DESCRIPTION
 
@@ -53,9 +91,5 @@ Manipulating mixin and inheritance outside of packages
 
 =head2 EXPORT
 
-&implant()
-
-=head1 SEE ALSO
-
-Class::Inspector
+implant()
 
